c1acdb7ce233126aa50a9f4b2be704285e8e6423
rustadadam <rustadadam@gmail.com>
2025-03-26 21:20:01 -0600

    First pipeline built. QUANT only

--- Pipeline/pipeline_functions.py
+++ Pipeline/pipeline_functions.py
@@ -1,6 +1,12 @@
 
 
-#Stubbed
-def data_to_proximities(time_series, model, static = None):
-    pass
-    #return proximites
+#Function to convert data to proximities
+def data_to_proximities(model, Xtrain, ytrain, Xtest, Xstatic_train = None, Xstatic_test = None):
+
+    #Fit model
+    model.fit(Xtrain, ytrain, static = Xstatic_train) #NOTE: May want to abstract this away later
+
+    #Get proximities
+    proximites = model.predict_proximities(Xtest, static = Xstatic_test) #NOTE: May want to abstract this away later
+
+    return proximites
