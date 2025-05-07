5d91bfd41d4e9059a27332b367805ead8ddd7f21
rustadadam <rustadadam@gmail.com>
2025-05-06 20:15:09 -0600

    Added More distance measures

--- independent_distance/distance_helpers.py
+++ independent_distance/distance_helpers.py
@@ -1,6 +1,7 @@
 # File for functions to implement distance calculations for times series
 
 from tslearn.metrics import cdist_dtw, cdist_soft_dtw, cdist_soft_dtw_normalized
+from sklearn.metrics.pairwise import euclidean_distances, manhattan_distances, cosine_distances
 
 def compute_distance_matrix(time_series_list, metric="dtw"):
     """
@@ -8,7 +9,8 @@ def compute_distance_matrix(time_series_list, metric="dtw"):
 
     Parameters:
         time_series_list (list): A list of time series data, where each time series is a 2D array.
-        metric (str): The distance metric to use. Options are "dtw", "soft_dtw", "soft_dtw_normalized".
+        metric (str): The distance metric to use. Options are "dtw", "soft_dtw", "soft_dtw_normalized",
+                      "euclidean", "manhattan", "cosine".
 
     Returns:
         numpy.ndarray: The computed distance matrix.
@@ -19,5 +21,11 @@ def compute_distance_matrix(time_series_list, metric="dtw"):
         return cdist_soft_dtw(time_series_list)
     elif metric == "soft_dtw_normalized":
         return cdist_soft_dtw_normalized(time_series_list)
+    elif metric == "euclidean":
+        return euclidean_distances(time_series_list)
+    elif metric == "manhattan":
+        return manhattan_distances(time_series_list)
+    elif metric == "cosine":
+        return cosine_distances(time_series_list)
     else:
-        raise ValueError("Unsupported metric. Choose from 'dtw', 'soft_dtw', or 'soft_dtw_normalized'.")
+        raise ValueError("Unsupported metric. Choose from 'dtw', 'soft_dtw', 'soft_dtw_normalized', 'euclidean', 'manhattan', or 'cosine'.")
