6237b515f868e6ac5884316766b4a629a2b53d4a
rustadadam <rustadadam@gmail.com>
2025-06-12 13:54:43 -0600

    Enabled distances measures

--- experiments/model_helpers.py
+++ experiments/model_helpers.py
@@ -12,11 +12,13 @@ from RFGAP_Rocket.RFGAP_Rocket import RFGAP_Rocket
 from QGAP.qgap import QGAP
 from Redcomets.Redcomets import REDCOMETS
 from FreshPrince.FreshPrince import FreshPRINCE_GAP
-
-
+from independent_distance.distance_helpers import compute_distance_matrix
 
 # Other imports
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+
 
 
 #& Model Get Prediction Methods
@@ -65,10 +67,14 @@ def get_qgap_pred(X_train, y_train, X_test, static_train, static_test, return_pr
     return qgap.predict(X_test, static = static_test)
 
     
-def get_redcomets_pred(X_train, y_train, X_test, static_train, static_test, **kwargs):
+def get_redcomets_pred(X_train, y_train, X_test, static_train, static_test, return_proximities = False, **kwargs):
     rc = REDCOMETS(static = static_train, variant=3, **kwargs)
     rc.fit(X_train, y_train)
-    return rc.predict(X_test, static = static_test), None, None
+
+    if return_proximities:
+        print("Proximities are not supported for Redcomets")
+
+    return rc.predict(X_test, static = static_test)
     
 
 def get_fresh_pred(X_train, y_train, X_test, static_train, static_test, return_proximities = False, **kwargs):
@@ -79,3 +85,27 @@ def get_fresh_pred(X_train, y_train, X_test, static_train, static_test, return_p
         return fp.predict(X_test, static = static_test), np.array(fp.get_proximities().todense()), np.array(fp.get_extend(X_test, static_test).todense())
     
     return fp.predict(X_test, static = static_test)
+
+#& Distance Based Methds
+
+def evaluate_knn_on_distance_matrix(distance_matrix, labels, n_neighbors=1):
+    
+    from sklearn.model_selection import StratifiedKFold
+
+    skf = StratifiedKFold(n_splits=2, shuffle=True, random_state=42)
+    
+    for fold, (train_idx, test_idx) in enumerate(skf.split(distance_matrix, labels)):
+        # Extract the relevant submatrices for precomputed distances
+        train_dist = distance_matrix[np.ix_(train_idx, train_idx)]
+        test_dist = distance_matrix[np.ix_(test_idx, train_idx)]
+        y_train, y_test = labels[train_idx], labels[test_idx]
+
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors, metric='precomputed')
+        knn.fit(train_dist, y_train)
+        y_pred = knn.predict(test_dist)
+        return y_pred, y_test
+
+def get_distance_pred(time_series, labels, metric = "dtw"):
+    distances = compute_distance_matrix(time_series, metric=metric)
+
+    return evaluate_knn_on_distance_matrix(distances, labels, n_neighbors=1)
